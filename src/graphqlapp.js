b9bb570bfe281491dc8f737f55674009dae0ccf9
moridio <moridio@gmail.com>
2024-09-25 22:41:01 +0300

    initial setup stuff

--- src/graphqlapp.js
+++ src/graphqlapp.js
@@ -1,15 +1,55 @@
-class PageHandling{
+const graphqlEndpoint = "https://01.kood.tech/api/graphql-engine/v1/graphql";
+const authEndpoinint = "https://01.kood.tech/api/auth/signin";
 
+let user = null;
+let pwd = null;
+
+let JWToken = null;
+
+let statsData = null;
+
+let loginSection = null;
+let graphSection = null; 
+let logoutSection = null; 
+
+let app = init();
+
+function init() {
+    console.log("ok can start");
+    preparePage();
+    displayLogin();
 }
 
-class Auth{
-
-}
-
-class DataHandling{
-
-}
-
-class GraphDraw{
+function preparePage(){
     
+}
+
+function displayLogin() {
+
+}
+
+function displayLogout(){
+    
+}
+
+function getAuthentication() {
+
+}
+
+function displayError(error) {
+
+}
+
+function displayGraphPage() {
+
+}
+
+function createProgressGraph(data) {
+    let graph = null;
+    return graph;
+}
+
+function createAuditGraph(data) {
+    let graph = null;
+    return graph;
 }
