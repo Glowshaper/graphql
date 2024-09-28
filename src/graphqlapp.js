d10f7f6d6602d1188f83496fed408aafa394614f
moridio <moridio@gmail.com>
2024-09-28 19:38:36 +0300

    wip

--- src/graphqlapp.js
+++ src/graphqlapp.js
@@ -11,6 +11,10 @@ let statsData = null;
 let loginSection = null;
 let graphSection = null; 
 let logoutSection = null; 
+let infoSection = null;
+
+let loginButton = null;
+let logoutButton = null;
 
 let app = init();
 
@@ -21,10 +25,41 @@ function init() {
 }
 
 function preparePage(){
-    
+    loginSection = Document.getElementById("login");
+    logoutSection = Document.getElementById("logout");
+    infoSection = Document.getElementById("info_text");
+    graphSection = Document.getElementById("graph_view");
+
+    //graphSection.innerHtml = "";
+    infoSection.innerText = "";
+    graphSection.styles.display = "none";
+    logoutSection.styles.display = "none";
 }
 
 function displayLogin() {
+    loginButton = Document.getElementById("submit_login");
+    loginButton.addEventListener("click", initiateLogin)
+
+}
+
+async function initiateLogin(){
+    infoSection.innerText = "";
+    user = Document.getElementById("name_input").value;
+    pwd = Document.getElementById("passwd_input").value;
+
+    //let login = btoa(`${user}:${pwd}`);
+    let login = btoa(user+":"+pwd);
+    console.log("login", login);
+    if(user !== "" && pwd !==""){
+        let response = await fetch(authEndpoinint, {
+            method: "POST",
+            //headers: {'Authorization': `Basic ${login}`}
+            headers: {'Authorization': "Basic "+login}
+            })
+         console.log("response",response);
+    }else{
+        infoSection.innerText = "Please provide both your KOOD/JÕHVI username or email address, and KOOD/JÕHVI password"
+    }
 
 }
 
